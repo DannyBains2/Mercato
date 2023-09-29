f9b503488fe5bc4a018e6d99e71bc6a78bdc668c
dannybains2 <dannybains2@gmail.com>
2023-09-29 14:32:40 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,221 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { TEInput, TERipple } from "tw-elements-react"
+import { useState } from "react";
+import Model from "./Model";
+
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false)
+
+const [first, setFirst] = useState("")
+const [second, setSecond] = useState("")
+const [house, setHouse] = useState("")
+const [number, setNumber] = useState("")
+const [street, setStreet] = useState("")
+const [city, setCity] = useState("")
+const [postcode, setPostcode] = useState("")
+const [module, setModule] = useState("")
+const [inverter, setInverter] = useState("")
+const [numModules, setNumModules] = useState("")
+const [cable, setCable] = useState("")
+const [numRow, setNumRow] = useState("")
+const [numCol, setNumCol] = useState("")
+const [delColl, setDelColl] = useState("")
+const [orientation, setOrientation] = useState("")
+
+const details = {
+  first,
+  second,
+  house,
+  street,
+  city,
+  postcode,
+  module : module.replace(/\s/g,''),
+  inverter: inverter.replace(/\s/g,''),
+  numModules: Number(numModules),
+  cable,
+  numRow: Number(numRow),
+  numCol: Number(numCol),
+  delColl,
+  orientation
+}
+function handleClick (){
+
+}
+// console.log (isOpen)
+//  console.log (details)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App w-full h-screen  flex relative">
+      <div className="w-1/2 h-auto   flex justify-center">
+      <div className="my-10 block max-w-2xl rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
+      <form>
+        <div className=" pl-3 w-full rounded-md border-4 border-[#4C1E4F]   text-white bg-[#4C1E4F] mb-4">
+          Your Details
+        </div>
+        <div className="grid grid-cols-3 gap-4">
+          {/* <!--First name input--> */}
+          <TEInput
+            type="text"
+            label="First name"
+            className="mb-6"
+            onChange={(e) => setFirst(e.target.value)}
+          ></TEInput>
+
+          {/* <!--Last name input--> */}
+          <TEInput
+            type="text"
+            label="Last name"
+            className="mb-6"
+            onChange={(e) => setSecond(e.target.value)}
+          ></TEInput>
+
+             {/* <!--House Name/Number input--> */}
+        <TEInput
+          type="text"
+          label="House Name/Number"
+          className="mb-6"
+          onChange={(e) => setHouse(e.target.value)}
+        ></TEInput>
+        </div>
+
+        <div className="grid grid-cols-3 gap-4">
+          {/* <!--Street Name input--> */}
+          <TEInput
+            type="text"
+            label="Street Name"
+            className="mb-6"
+            onChange={(e) => setStreet(e.target.value)}
+          ></TEInput>
+
+          {/* <!--Town/City input--> */}
+          <TEInput
+            type="text"
+            label="Town/City"
+            className="mb-6"
+            onChange={(e) => setCity(e.target.value)}
+          ></TEInput>
+
+             {/* <!--Postcode input--> */}
+        <TEInput
+          type="text"
+          label="Postcode"
+          className="mb-6"
+          onChange={(e) => setPostcode(e.target.value)}
+        ></TEInput>
+        </div>
+
+        <br></br>
+        <hr></hr>
+        <br></br>
+
+        <div className="  pl-3 w-full rounded-md border-4 border-[#4C1E4F]   text-white bg-[#4C1E4F] mb-4">
+          Your Order
+        </div>
+
+        <div className="grid grid-cols-3 gap-4">
+          {/* <!--Module type input--> */}
+          <select id="cars" name="cars" className="mb-6 border border-gray-200" onChange={(e) => setModule(e.target.value)}>
+            <option value="none" selected disabled hidden>Type of module</option>
+            <option value="Module A">Module A</option>
+            <option value="Module B">Module B</option>
+          </select>
+
+          {/* <!--Type of Inverter input--> */}
+          <select id="cars" name="cars" className="mb-6 border border-gray-200" onChange={(e) => setInverter(e.target.value)}>
+          <option value="none" selected disabled hidden>Type of Inverter</option>
+            <option value="Inverter A">Inverter A</option>
+            <option value="Inverter B">Inverter B</option>
+            <option value="Inverter C">Inverter C</option>
+          </select>
+
+             {/* <!--Number Of modules/Number input--> */}
+        <TEInput
+          type="number"
+          label="Number of modules"
+          className="mb-6 "
+          onChange={(e) => setNumModules(e.target.value)}
+        ></TEInput>
+        </div>
+
+        <div className="grid grid-cols-3 gap-4">
+
+        <select id="cars" name="cars" className="mb-6 border border-gray-200" onChange={(e) => setCable(e.target.value)}>
+        <option value="none" selected disabled hidden>Cable length in meters</option>
+            <option value="10">10</option>
+            <option value="20">20</option>
+            <option value="30">30</option>
+            <option value="40">40</option>
+            <option value="50">50</option>
+            <option value="60">60</option>
+            <option value="70">70</option>
+            <option value="80">80</option>
+            <option value="90">90</option>
+            <option value="100">100</option>
+            <option value="110">110</option>
+            <option value="120">120</option>
+            <option value="130">130</option>
+            <option value="140">140</option>
+            <option value="150">150</option>
+          </select>
+
+
+          {/* <!--Number of columns input--> */}
+          <TEInput
+            type="number"
+            label="Number of columns"
+            className="mb-6"
+            onChange={(e) => setNumCol(e.target.value)}
+          ></TEInput>
+
+<select id="cars" name="cars" className="mb-6 border border-gray-200" onChange={(e) => setOrientation(e.target.value)}>
+            <option value="none" selected disabled hidden>Select Orientation</option>
+            <option value="Landscape">Landscape</option>
+            <option value="Portrait">Portrait</option>
+          </select>
+        </div>
+
+        <div className="grid grid-cols-3 gap-4">
+                 {/* <!--number of rows input--> */}
+                 <TEInput
+            type="number"
+            label="Number of rows"
+            className="mb-6"
+            onChange={(e) => setNumRow(e.target.value)}
+          ></TEInput>
+
+          <select id="cars" name="cars" className="mb-6 border border-gray-200" onChange={(e) => setDelColl(e.target.value)}>
+          <option value="none" selected disabled hidden>Delivery/Collection</option>
+            <option value="Delivery">Delivery</option>
+            <option value="Collection">Collection</option>
+          </select>
+
+        </div>
+
+        
+        {/* <!--Subscribe newsletter checkbox--> */}
+
+
+        {/* <!--Submit button--> */}
+        <TERipple rippleColor="light" className="w-full">
+          <button
+            onClick = {() => setIsOpen(!isOpen)}
+            type="button"
+            className="block w-auto rounded bg-primary-900 px-6 pb-2 pt-2.5 text-base font-bold uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]] "
+          >
+            Get Quote
+          </button>
+        </TERipple>
+      </form>
+    </div>
+      </div>
+
+      { isOpen ? <Model details = {details} setIsOpen = {setIsOpen} isOpen = {isOpen}/>
+ : ""}
+
+      <div className="w-1/2 bg-[#f5b82e] flex flex-col items-center justify-center">
+        <h1 className=" text-8xl">Mercato</h1>
+        <h3 className=' text-2xl'>Solar Panel Inc.</h3>
+      </div>
     </div>
   );
 }
---
+++ src/Model.js
@@ -0,0 +1,268 @@
+import { useRef } from "react"
+import { useReactToPrint } from 'react-to-print'
+
+export default function Model ({details,setIsOpen, isOpen}){
+        
+const printRef= useRef()
+
+const handlePrint = useReactToPrint({
+  content: () => printRef.current
+})
+const handleClick = ()=>{
+  alert("I am an alert box!")
+  setIsOpen(!isOpen)
+}
+
+        const parts = {
+            Bolts: 0.05,
+            Screws:	0.10,
+            Washers: 0.03,
+            Rails15m: 24.50,
+            Rails1m: 18.00,
+            Rails05m: 9.00,
+            ACIsolator: 67.00,
+            DCIsolator: 122.00,
+            WarningLabels: 2.00,
+            Hooks: 1.24,
+            Cable: 3.00,
+            ModuleA: 450.00,
+            ModuleB: 650.00,
+            InverterA: 500.00,
+            InverterB: 600.00,
+            InverterC: 700.00,
+        }
+
+        const qty = {
+            Bolts: 0,
+            Screws:	0,
+            Washers: 0,
+            Rails15m: 0,
+            Inverter: 0,
+            Rails1m: 0,
+            Rails05m: 0,
+            ACIsolator: 0,
+            DCIsolator: 0,
+            WarningLabels: 0,
+            Hooks: 0,
+            Cable: 0,
+            Module: 0,
+        }
+     
+        
+        qty.Module = details.numModules
+        qty.Inverter = 1
+        qty.DCIsolator = qty.Inverter
+        qty.ACIsolator = qty.Inverter*2
+        qty.WarningLabels = qty.ACIsolator*4
+
+        if (details.orientation === "Portrait"){
+            qty.Hooks = (details.numRow*2)*(details.numCol+1)
+            let rail15m = details.numCol * 1640 + (details.numCol-1)
+            qty.Rails15m = Math.floor(rail15m/1500)
+            let remainder = rail15m%1500
+            if (remainder >= 1000){
+                qty.Rails15m = qty.Rails15m + 1
+            }
+            if(remainder > 500 && remainder < 1000){
+                qty.Rails1m = qty.Rails1m + 1
+            }
+            if(remainder > 0 && remainder < 500){
+                qty.Rails05m = qty.Rails05m + 1
+            }
+            qty.Rails15m = qty.Rails15m * (details.numRow*2)
+            qty.Rails1m = qty.Rails1m * (details.numRow*2)
+            qty.Rails05m = qty.Rails05m * (details.numRow*2)
+
+        } else {
+            qty.Hooks = (details.numCol*2)*(details.numRow+1)
+            let rail15m = details.numRow * 1640 + (details.numRow-1)
+            qty.Rails15m = Math.floor(rail15m/1500)
+            qty.Rails15m = Math.floor(rail15m/1500)
+            let remainder = rail15m%1500
+            if (remainder >= 1000){
+                qty.Rails15m = qty.Rails15m + 1
+            }
+            if(remainder > 500 && remainder < 1000){
+                qty.Rails1m = qty.Rails1m + 1
+            }
+            if(remainder > 0 && remainder < 500){
+                qty.Rails05m = qty.Rails05m + 1
+            }
+            qty.Rails15m = qty.Rails15m * (details.numRow*2)
+            qty.Rails1m = qty.Rails1m * (details.numRow*2)
+            qty.Rails05m = qty.Rails05m * (details.numRow*2)
+            
+        }
+        qty.Cable = Number(details.cable/10)
+        qty.Bolts = details.numModules*2
+        qty.Screws = Math.ceil(qty.Hooks*2.25)
+        qty.Washers = qty.Screws
+       
+       let totalExc = Number(((parts[details.module])*details.numModules).toFixed(2))+Number((parts[details.inverter]).toFixed(2))+Number(((parts.DCIsolator)*qty.DCIsolator).toFixed(2))+Number(((parts.ACIsolator)*qty.ACIsolator).toFixed(2))+Number(((parts.Bolts)*qty.Bolts).toFixed(2))+Number(((parts.Screws)*qty.Screws).toFixed(2))+Number(((parts.Washers)*qty.Washers).toFixed(2))+Number(((parts.Rails15m)*qty.Rails15m).toFixed(2))+Number(((parts.Rails1m)*qty.Rails1m).toFixed(2))+Number(((parts.Rails05m)*qty.Rails05m).toFixed(2))+Number(((parts.WarningLabels)*qty.WarningLabels).toFixed(2))+ Number(((parts.Hooks)*qty.Hooks).toFixed(2))+Number(((parts.Cable)*qty.Cable).toFixed(2))
+            totalExc = (totalExc).toFixed(2)
+        let vat = (totalExc/5).toFixed(2)
+        console.log (qty, details.numCol, qty.Module)
+    return(
+        <>
+        
+
+
+        <div ref={printRef} className=" w-5/6 h-5/6 border-4 border-black absolute bg-white flex-col ">
+
+        <div className="w-full h-14 border text-white font-bold text-4xl bg-[#4c1e4f] flex justify-center">
+            Mercato Solar Panel Inc.
+        </div>
+
+        <div className=" w-full h-32 border  flex justify-between">
+            <div className="border h-full w-1/4 bg-[#4c1e4f] flex flex-col overflow-auto text-white text-sm pl-3">
+                
+                <div>{details.first}</div>
+                <div>{details.second}</div>
+                <div>{details.house}</div>
+                <div>{details.street}</div>
+                <div>{details.city}</div>
+                <div>{details.postcode}</div>
+            </div>
+            <div className="border h-full w-1/4 bg-[#4c1e4f] overflow-auto text-white text-4xl pl-3">
+              <div>This order is for:</div>
+            <div>{details.delColl}</div>
+            </div>
+        </div>
+        <table className="secondTable w-full border text-xs bg-white">
+              <thead>
+                <tr >
+                  <th className="border"><span >Qty</span></th>
+                  <th className="border"><span >Part</span></th>
+                  <th className="border"><span >Single £</span></th>
+                  <th className="border"><span >Total £</span></th>
+                  
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className="border text-center"><a class="cut">{qty.Module}</a><span ></span></td>
+                  <td className="border text-center w-3/5"><span >{details.module}</span></td>
+                  <td className="border text-center"><span data-prefix>£</span><span >{(parts[details.module]).toFixed(2)}</span></td>
+                  <td  className="border text-center"><span data-prefix>£</span><span >{((parts[details.module])*details.numModules).toFixed(2)}</span></td>
+                  
+                </tr>
+                <tr>
+                  <td className="border text-center"><a class="cut">{qty.Inverter}</a><span ></span></td>
+                  <td className="border text-center"><span >{details.inverter}</span></td>
+                  <td className="border text-center"><span data-prefix>£</span><span >{(parts[details.inverter]).toFixed(2)}</span></td>
+                  <td  className="border text-center"><span data-prefix>£</span><span >{(parts[details.inverter]).toFixed(2)}</span></td>
+                  
+                </tr>
+                <tr>
+                  <td className="border text-center"><a class="cut">{qty.DCIsolator}</a><span ></span></td>
+                  <td className="border text-center"><span >DC Isolator</span></td>
+                  <td className="border text-center"><span data-prefix>£</span><span >{(parts.DCIsolator).toFixed(2)}</span></td>
+                  <td  className="border text-center"><span data-prefix>£</span><span >{((parts.DCIsolator)*qty.DCIsolator).toFixed(2)}</span></td>
+                  
+                </tr>
+                <tr>
+                  <td className="border text-center"><a class="cut">{qty.ACIsolator}</a><span ></span></td>
+                  <td className="border text-center"><span >AC Isolator</span></td>
+                  <td className="border text-center"><span data-prefix>£</span><span >{(parts.ACIsolator).toFixed(2)}</span></td>
+                  <td  className="border text-center"><span data-prefix>£</span><span >{((parts.ACIsolator)*qty.ACIsolator).toFixed(2)}</span></td>
+                  
+                </tr>
+                <tr>
+                  <td className="border text-center"><a class="cut">{qty.Bolts}</a><span ></span></td>
+                  <td className="border text-center"><span >Bolts</span></td>
+                  <td className="border text-center"><span data-prefix>£</span><span >{(parts.Bolts).toFixed(2)}</span></td>
+                  <td  className="border text-center"><span data-prefix>£</span><span >{((parts.Bolts)*qty.Bolts).toFixed(2)}</span></td>
+                  
+                </tr>
+                <tr>
+                  <td className="border text-center"><a class="cut">{qty.Screws}</a><span ></span></td>
+                  <td className="border text-center"><span >Screws</span></td>
+                  <td className="border text-center"><span data-prefix>£</span><span >{(parts.Screws).toFixed(2)}</span></td>
+                  <td  className="border text-center"><span data-prefix>£</span><span >{((parts.Screws)*qty.Screws).toFixed(2)}</span></td>
+                  
+                </tr>
+                <tr>
+                  <td className="border text-center"><a class="cut">{qty.Washers}</a><span ></span></td>
+                  <td className="border text-center"><span >Washers</span></td>
+                  <td className="border text-center"><span data-prefix>£</span><span >{(parts.Washers).toFixed(2)}</span></td>
+                  <td  className="border text-center"><span data-prefix>£</span><span >{((parts.Washers)*qty.Washers).toFixed(2)}</span></td>
+                  
+                </tr>
+                <tr>
+                  <td className="border text-center"><a class="cut">{qty.Rails15m}</a><span ></span></td>
+                  <td className="border text-center"><span >Rails 1.5M</span></td>
+                  <td className="border text-center"><span data-prefix>£</span><span >{(parts.Rails15m).toFixed(2)}</span></td>
+                  <td  className="border text-center"><span data-prefix>£</span><span >{((parts.Rails15m)*qty.Rails15m).toFixed(2)}</span></td>
+                  
+                </tr>
+                <tr>
+                  <td className="border text-center"><a class="cut">{qty.Rails1m}</a><span ></span></td>
+                  <td className="border text-center"><span >Rails 1M</span></td>
+                  <td className="border text-center"><span data-prefix>£</span><span >{(parts.Rails1m).toFixed(2)}</span></td>
+                  <td  className="border text-center"><span data-prefix>£</span><span >{((parts.Rails1m)*qty.Rails1m).toFixed(2)}</span></td>
+                  
+                </tr>
+                <tr>
+                  <td className="border text-center"><a class="cut">{qty.Rails05m}</a><span ></span></td>
+                  <td className="border text-center"><span >Rails 0.5M</span></td>
+                  <td className="border text-center"><span data-prefix>£</span><span >{(parts.Rails05m).toFixed(2)}</span></td>
+                  <td  className="border text-center"><span data-prefix>£</span><span >{((parts.Rails05m)*qty.Rails05m).toFixed(2)}</span></td>
+                  
+                </tr>
+                <tr>
+                  <td className="border text-center"><a class="cut">{qty.WarningLabels}</a><span ></span></td>
+                  <td className="border text-center"><span >Warning Labels</span></td>
+                  <td className="border text-center"><span data-prefix>£</span><span >{(parts.WarningLabels).toFixed(2)}</span></td>
+                  <td  className="border text-center"><span data-prefix>£</span><span >{((parts.WarningLabels)*qty.WarningLabels).toFixed(2)}</span></td>
+                  
+                </tr>
+                <tr>
+                  <td className="border text-center"><a class="cut">{qty.Hooks}</a><span ></span></td>
+                  <td className="border text-center"><span >Hooks</span></td>
+                  <td className="border text-center"><span data-prefix>£</span><span >{parts.Hooks}</span></td>
+                  <td  className="border text-center"><span data-prefix>£</span><span >{((parts.Hooks)*qty.Hooks).toFixed(2)}</span></td>
+                  
+                </tr>
+                <tr>
+                  <td className="border text-center"><a class="cut">{qty.Cable}</a><span ></span></td>
+                  <td className="border text-center"><span >Cable (10m)</span></td>
+                  <td className="border text-center"><span data-prefix>£</span><span >{(parts.Cable).toFixed(2)}</span></td>
+                  <td  className="border text-center"><span data-prefix>£</span><span >{((parts.Cable)*qty.Cable).toFixed(2)}</span></td>
+                  
+                </tr>
+                <tr>
+                  <td className="border text-center"><a class="cut"></a><span ></span></td>
+                  <td className="border text-center"><span ></span></td>
+                  <td className="border text-center"><span data-prefix></span><span >Total Exc VAT</span></td>
+                  <td  className="border text-center"><span data-prefix>£</span><span >{totalExc}</span></td>
+                  
+                </tr>
+                <tr>
+                  <td className="border text-center"><a class="cut"></a><span ></span></td>
+                  <td className="border text-center"><span ></span></td>
+                  <td className="border text-center"><span data-prefix></span><span >VAT 20%</span></td>
+                  <td  className="border text-center"><span data-prefix>£</span><span >{vat}</span></td>
+                  
+                </tr>
+                <tr>
+                  <td className="border text-center"><a class="cut"></a><span ></span></td>
+                  <td className="border text-center"><span ></span></td>
+                  <td className="border text-center font-bold"><span data-prefix></span><span >Total Inc VAT</span></td>
+                  <td  className="border text-center"><span data-prefix>£</span><span >{(Number(vat)+Number(totalExc)).toFixed(2)}</span></td>
+                  
+                </tr>
+              </tbody>
+            </table>
+        
+           <div className=" grid grid-cols-3 gap-32 mt-10">
+
+              
+                <button className="bg-[#f5b82e] hover:bg-[#E8AE2A] text-white font-bold py-2 px-4 rounded" onClick={handlePrint}>Print</button>
+                <button className="bg-[#f5b82e] hover:bg-[#E8AE2A] text-white font-bold py-2 px-4 rounded" onClick={handleClick}>Place Order</button>
+                <button className="bg-[#f5b82e] hover:bg-[#E8AE2A] text-white font-bold py-2 px-4 rounded" onClick={()=>setIsOpen(!isOpen)}>Close</button>
+           </div>
+
+        </div>
+        </>
+    )
+}
